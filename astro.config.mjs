6b0bd25eef55e6ef93641d75aa931f6990d0813a
JairVazquez <jairvazquez604@gmail.com>
2024-12-12 21:29:04 -0600

    Add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: vercel()
 });
